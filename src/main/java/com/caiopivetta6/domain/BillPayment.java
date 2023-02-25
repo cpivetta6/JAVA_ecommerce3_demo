7de65f790867721ebb6abad2a65ab2c84eda990a
Caio Pivetta <caiopivetta6@gmail.com>
2023-02-25 10:08:05 +0100

    Payment, Billpayment, CardPayment

--- src/main/java/com/caiopivetta6/domain/BillPayment.java
+++ src/main/java/com/caiopivetta6/domain/BillPayment.java
@@ -1,5 +1,34 @@
 package com.caiopivetta6.domain;
 
-public class BillPayment {
+import java.time.Instant;
 
+import com.caiopivetta6.domain.enums.StatePayment;
+
+public class BillPayment extends Payment {
+
+	private Instant datePayment;
+	
+	public BillPayment() {
+		
+	}
+
+	public BillPayment(Integer id, StatePayment state, Instant datePayment) {
+		super(id, state);
+		this.datePayment = datePayment;
+		// TODO Auto-generated constructor stub
+	}
+
+	public Instant getDatePayment() {
+		return datePayment;
+	}
+
+	public void setDatePayment(Instant datePayment) {
+		this.datePayment = datePayment;
+	}
+	
+	
+	
+	
+	
+	
 }
--- src/main/java/com/caiopivetta6/domain/CardPayment.java
+++ src/main/java/com/caiopivetta6/domain/CardPayment.java
@@ -1,5 +1,33 @@
 package com.caiopivetta6.domain;
 
-public class CardPayment {
+import java.time.Instant;
 
+import com.caiopivetta6.domain.enums.StatePayment;
+
+public class CardPayment extends Payment {
+
+	private Instant date_payment;
+	
+	public CardPayment() {
+		
+	}
+
+	public CardPayment(Integer id, StatePayment state, Instant date_payment) {
+		super(id, state);
+		this.date_payment = date_payment;
+		
+	}
+
+	public Instant getDate_payment() {
+		return date_payment;
+	}
+
+	public void setDate_payment(Instant date_payment) {
+		this.date_payment = date_payment;
+	}
+	
+	
+	
+	
+	
 }
